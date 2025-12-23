723e385a9c048c24f34253fe41a829baea3fd04a
ivo448 <ignacio.valdiviaoyarce@gmail.com>
2025-12-22 22:12:08 -0300

    logs

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // Importamos los estilos de Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+console.log("ENV VARS:", import.meta.env);
+console.log("API URL DETECTADA:", import.meta.env.VITE_API_BASE_URL);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
